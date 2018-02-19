30538d233e66bb633f3b83abc8548869ca322734
Aliaksei Labkovich <aliaksei.labkovich@itechart-group.com>
2018-02-19 16:13:47 +0300

    Implementing color constants

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './styles.css';
+import styles from './styles.scss';
 
 const Header = () => (
         <div className={styles.header}>
--- src/components/movie/movie-card/movie-card.js
+++ src/components/movie/movie-card/movie-card.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './styles.css';
+import styles from './styles.scss';
 
 
 const Movie = ({ id, posterUrl, title }) => (
         // TODO push to history on click instead of Link
-        <Link to={`/movies/${id}`} className={`${styles.container} col-sm`}>
+        <Link to={`/movies/${id}`} className={styles.container}>
             <img src={posterUrl} className={styles.poster}/>
             <div className={styles.title}>
                 {title}
